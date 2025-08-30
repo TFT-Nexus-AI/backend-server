082f746f51e5538bad0530962428d862897b6aa4
LJJ <wjdrk70@gmail.com>
2025-08-30 16:46:17 +0900

    feat: baseEntity 추가

--- storage/db-core/src/main/java/org/project/storage/db/core/match/MatchEntity.java
+++ storage/db-core/src/main/java/org/project/storage/db/core/match/MatchEntity.java
@@ -5,16 +5,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.project.storage.db.core.BaseEntity;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "matches")
-public class MatchEntity {
+public class MatchEntity extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
 	@Column(unique = true, nullable = false, length = 50)
 	private String matchId;
--- storage/db-core/src/main/java/org/project/storage/db/core/user/UserEntity.java
+++ storage/db-core/src/main/java/org/project/storage/db/core/user/UserEntity.java
@@ -3,6 +3,8 @@ package org.project.storage.db.core.user;
 import jakarta.persistence.*;
 import lombok.*;
 import org.project.domain.user.User;
+import org.project.storage.db.core.BaseEntity;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -10,48 +12,45 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = "puuid")
-public class UserEntity {
+public class UserEntity extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
-	@Column(name = "puuid", unique = true, nullable = false, length = 100)
-	private String puuid;
+    @Column(name = "puuid", unique = true, nullable = false, length = 100)
+    private String puuid;
 
-	@Column(name = "game_name", nullable = false, length = 50)
-	private String gameName;
+    @Column(name = "game_name", nullable = false, length = 50)
+    private String gameName;
 
-	@Column(name = "tag_line", nullable = false, length = 10)
-	private String tagLine;
+    @Column(name = "tag_line", nullable = false, length = 10)
+    private String tagLine;
 
-	@Column(name = "created_at", nullable = false)
-	private LocalDateTime createdAt;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 
-	@Builder
-	private UserEntity(String puuid, String gameName, String tagLine, LocalDateTime createdAt) {
-		this.puuid = puuid;
-		this.gameName = gameName;
-		this.tagLine = tagLine;
-		this.createdAt = createdAt;
-	}
+    @Builder
+    private UserEntity(String puuid, String gameName, String tagLine, LocalDateTime createdAt) {
+        this.puuid = puuid;
+        this.gameName = gameName;
+        this.tagLine = tagLine;
+        this.createdAt = createdAt;
+    }
 
-	public User toDomain() {
-		return User.builder()
-			.puuid(this.puuid)
-			.gameName(this.gameName)
-			.tagLine(this.tagLine)
-			.createdAt(this.createdAt)
-			.build();
-	}
+    public User toDomain() {
+        return User.builder()
+                .puuid(this.puuid)
+                .gameName(this.gameName)
+                .tagLine(this.tagLine)
+                .createdAt(this.createdAt)
+                .build();
+    }
 
-	public static UserEntity fromDomain(User user) {
-		return UserEntity.builder()
-			.puuid(user.getPuuid())
-			.gameName(user.getGameName())
-			.tagLine(user.getTagLine())
-			.createdAt(user.getCreatedAt())
-			.build();
-	}
+    public static UserEntity fromDomain(User user) {
+        return UserEntity.builder()
+                .puuid(user.getPuuid())
+                .gameName(user.getGameName())
+                .tagLine(user.getTagLine())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
 
 }
